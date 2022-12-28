6c06e9eb333afc04a4fdba375c71627667c9d500
yusi4 <yusi4@DESKTOP-E80JV74>
2022-12-29 01:17:15 +0400

    Add project files.

---
+++ tasks/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace tasks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1.Cüt yerdə duran rəqəmlər
+            //Verilmiş ədədin cüt yerdə duran rəqəmlərinin kvadratını  hesablayan proqramı yazın.
+            //Giris: 123046
+            //Cixis: 40
+            //Izahi: 2 * 2 + 0 * 0 + 6 * 6 = 40
+
+            //Console.Write("eded daxil edin:");
+            //int number = int.Parse(Console.ReadLine());
+            //int r = 0, count = 0;
+
+            //while (number != 0)
+            //{
+            //    count = (count + r) * 10;
+            //    r = number % 10;
+            //    number = number / 10;
+
+            //}
+            //int reverseNumber = count + r;
+
+            //int sum = 0, a = 0, b;
+            //while (reverseNumber != 0)
+            //{
+            //    b = reverseNumber % 10;
+            //    reverseNumber /= 10;
+            //    if (a % 2 != 0)
+            //    {
+            //        sum = sum + b * b;
+            //    }
+            //    a++;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+            #region 2.Rəqəmlər və sıra nömrələri
+            //Daxil edilən ədədin rəqəmləri və onların
+            //sıra nömrələrinə olan hasilinin
+            //cəmini hesablayan proqram yazın
+            //Giriş: 2345
+            //Çıxış: 40
+            //Izahi: 1 * 2 + 3 * 2 + 3 * 4 + 4 * 5 = 40
+
+
+            //Console.Write("eded daxil edin:");
+            //int number = int.Parse(Console.ReadLine());
+            //int r = 0, count = 0;
+            //while (number != 0)
+            //{
+            //    count = (count + r) * 10;
+            //    r = number % 10;
+            //    number = number / 10;
+
+            //}
+            //int reverseNumber = count + r;
+            //int a, sum = 0, i = 1;
+
+            //while (reverseNumber != 0)
+            //{
+            //    a = reverseNumber % 10;
+            //    reverseNumber = reverseNumber / 10;
+            //    sum = sum + a * i;
+            //    i++;
+            //}
+            //Console.WriteLine(sum);
+            #endregion
+
+       
+        }
+    }
+}
